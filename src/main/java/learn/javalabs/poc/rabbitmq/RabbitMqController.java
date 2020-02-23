6f0623e4cd4a3d624aff269cdf6485bb30a33cb5
Girish Bathija <girish.bathija@upgrad.com>
2020-02-23 23:49:10 +0530

    Seggregating producer & consumer code in respective profiles

--- src/main/java/learn/javalabs/poc/rabbitmq/RabbitMqController.java
+++ src/main/java/learn/javalabs/poc/rabbitmq/RabbitMqController.java
@@ -2,6 +2,7 @@ package learn.javalabs.poc.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping(value = "/mq-publisher")
+@Profile("producer")
 public class RabbitMqController {
 
 	@Autowired
--- src/main/java/learn/javalabs/poc/rabbitmq/RabbitMqReceiver.java
+++ src/main/java/learn/javalabs/poc/rabbitmq/RabbitMqReceiver.java
@@ -3,11 +3,13 @@ package learn.javalabs.poc.rabbitmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("consumer")
 public class RabbitMqReceiver {
 
 	@RabbitListener(queues = {"#{nonSelectiveQueue.name}","#{selectiveQueue.name}","#{circleQueue.name}","#{squareQueue.name}"})
